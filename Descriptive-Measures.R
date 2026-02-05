e329d2ca97f47056e47b1782eeb9401d480646ae
Pawar <pawar1550@gmail.com>
2026-02-05 09:53:43 -0500

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,10 @@
+# Author: Pawar, Date: 02/05/2026, Purpose: Test descriptive measures
+
+# Create variable called "variablenew" holding 100 values from 100 to 200.
+# Calculate mean on variablenew for points 150 to 200
+# Calculate median on variablenew
+#  Calculate mean on variablenew for points 100 to 150
+
+Save the outputs after each function in this file, upload this file on your github repository to get full points.
+
+
